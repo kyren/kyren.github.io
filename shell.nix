1d61fb745d1d04ed8ba41e3c0d58e12826c24d32
Catherine West <kerriganw@gmail.com>
2018-09-28 23:43:49 -0400

    trying to get a wasm-bindgen build to work

--- shell.nix
+++ shell.nix
@@ -1,6 +1,13 @@
 with import <nixpkgs> {};
-
 let
+  moz_overlay = import (
+    builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
+  );
+
+  nixpkgs = import <nixpkgs> {
+    overlays = [ moz_overlay ];
+  };
+
   jekyll_env = bundlerEnv rec {
     name = "jekyll_env";
     ruby = ruby_2_5;
@@ -8,18 +15,45 @@ let
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
   };
-  moz_overlay = import (
-    builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
-  );
-  nixpkgs = import <nixpkgs> {
-    overlays = [ moz_overlay ];
-  };
+
   rust = (nixpkgs.rustChannelOf {
     date = "2018-09-24";
     channel = "nightly";
   }).rust.override {
     targets = [ "wasm32-unknown-unknown" ];
     extensions = [ "rust-src" ];
+  };
+
+  wasm-bindgen-version = "0.2.23";
+  wasm-bindgen-lockfile = ./_scripts/wasm-bindgen-Cargo.lock;
+
+  wasm-bindgen-src = stdenv.mkDerivation rec {
+    version = wasm-bindgen-version;
+    name = "wasm-bindgen-src-${version}";
+
+    src = fetchFromGitHub {
+      owner = "rustwasm";
+      repo = "wasm-bindgen";
+      rev = "${version}";
+      sha256 = "0bf4iya71ikci2asmynmj7yi1y2happqif1aq8y808jmalfk9f37";
+    };
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out/
+      cp ${wasm-bindgen-lockfile} $out/Cargo.lock
+      touch $out/crates/cli/Cargo.lock
+    '';
+  };
+
+  wasm-bindgen-cli = rustPlatform.buildRustPackage rec {
+    version = wasm-bindgen-version;
+    name = "wasm-bindgen-cli-${wasm-bindgen-version}";
+
+    src = wasm-bindgen-src;
+    sourceRoot = "${src.name}/crates/cli";
+
+    cargoSha256 = "16wxnx34svkjiik70hm3gr0z8pvgarrd00hkbh8gs1kpskd87jp4";
   };
 in
   with nixpkgs;
@@ -30,6 +64,6 @@ in
       jekyll_env
       rust
       rustracer
-      cargo-web
+      wasm-bindgen-cli
     ];
   }
